0d40ffadb47d163896a3c19d624e3fa75dffd9b4
mixfix12 <34396540+mixfix12@users.noreply.github.com>
2019-04-06 22:02:43 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php
 
-define('BOT_BASE_DIRECTORY', '/var/www');
-define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
-define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
-define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
-
-define('CALLBACK_API_CONFIRMATION_TOKEN', 'bb112c41'); //Строка для подтверждения адреса сервера из настроек Callback API
-define('VK_API_ACCESS_TOKEN', '016cb55230eb83e34f620389s149345d975bc12e2fb6f30e0a833a51d345d0d123c3dc0abc1c864036sdf989fb8345'); //Ключ доступа сообщества
+define('CALLBACK_API_CONFIRMATION_TOKEN', '49892080'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', 'b3bfae3ba7804f225fad0403f0a1ee66963cea016abead62288a20e78987299dfbd3e5dc39aa48109310b'); //Ключ доступа сообщества
 define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
